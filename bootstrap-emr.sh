f068a83a35e745dc64e1d266107ac8facd4bcfae
AnniRanok <148817416+AnniRanok@users.noreply.github.com>
2023-11-12 13:43:08 +0100

    Add files via upload

---
+++ bootstrap-emr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo python3 -m pip install -U setuptool
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install wheel
+sudo python3 -m pip install pillow
+sudo python3 -m pip install pandas=2.0.0
+sudo python3 -m pip install pyarrow
+sudo python3 -m pip install boto3
+
