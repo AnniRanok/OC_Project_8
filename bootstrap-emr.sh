ccdbcff18ca3f808e61d9a69b77be3d13155e788
AnniRanok <148817416+AnniRanok@users.noreply.github.com>
2023-11-18 02:25:18 +0100

    Update bootstrap-emr.sh

--- bootstrap-emr.sh
+++ bootstrap-emr.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo python3 -m pip install -U setuptool
-sudo python3 -m pip install -U pip
+sudo python3 -m pip install -U setuptools
 sudo python3 -m pip install wheel
 sudo python3 -m pip install pillow
-sudo python3 -m pip install pandas=2.0.0
+sudo python3 -m pip install pandas
 sudo python3 -m pip install pyarrow
 sudo python3 -m pip install boto3
-
+sudo python3 -m pip install tensorflow
+sudo python3 -m pip install scikit-learn
+sudo python3 -m pip install matplotlib
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install numpy 
